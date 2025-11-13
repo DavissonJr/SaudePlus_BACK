6a63af191ce5686c36b0a625beec5d9a2d9c3866
Dávisson Falcão <davissonbeat@hotmail.com>
2025-11-13 13:53:46 -0300

    feat: novos domains

---
+++ SAUDEPLUS_BACK/Domain/Agendamento.cs
@@ -0,0 +1,17 @@
+﻿namespace SAUDEPLUS_BACK.Domain;
+public class Agendamento
+{
+    public Guid Id { get; set; }
+    public string Nome { get; set; }
+    public string Email { get; set; }
+    public string Telefone { get; set; }
+    public Guid TipoConsultaId { get; set; }
+    public string PreferenciaHorario { get; set; }
+    public string Modalidade { get; set; }
+    public string Mensagem { get; set; }
+    public string Status { get; set; } = "pendente";
+    public DateTime DataCriacao { get; set; }
+    public DateTime DataAtualizacao { get; set; }
+
+    public virtual TipoConsulta TipoConsulta { get; set; }
+}
---
+++ SAUDEPLUS_BACK/Domain/TipoConsulta.cs
@@ -0,0 +1,11 @@
+﻿namespace SAUDEPLUS_BACK.Domain;
+
+public class TipoConsulta
+{
+    public Guid Id { get; set; }
+    public string Nome { get; set; }
+    public string Descricao { get; set; }
+    public int DuracaoMinutos { get; set; }
+    public bool Ativo { get; set; }
+    public DateTime DataCriacao { get; set; }
+}
